b7225bbdb3cb1254465edd3fbd89d498aa4d627b
Dionessaobi <dionessaobi@gmail.com>
2022-10-16 12:32:28 -0700

    formatting task

---
+++ print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * _formatter - a function checks if the character pointer 
+ * is NULL
+ * @args: a pointer that is initiated by va_list
+ * @format: a pointer variable that points to _printf
+ * @print_a: the array of structs that is initiated in the _printf function
+ *
+ * Return: 0. else return number of characters printed
+ */
+
