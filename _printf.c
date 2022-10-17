bb21e0d75bfe0975b01be662191cf81f81e2da87
Dionessaobi <dionessaobi@gmail.com>
2022-10-17 14:36:59 -0700

    _printf.c file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: formats the sting containing the character
+ * and the specifier c & s
+ *
+ * Return: the length of the formatted output string.
+ */
+int _printf(const char *format, ...)
+{
+
+}
